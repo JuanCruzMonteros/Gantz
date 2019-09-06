69645df496999aa462e6a86ac5f9151366bfcd79
jjuancruzrey <jjuancruzrey@gmail.com>
2019-09-06 09:13:18 -0300

    Clases de security Auth(falta agregar llaves RSA)

---
+++ back-apirest/src/main/java/com/back/security/auth/JwtConfig.java
@@ -0,0 +1,10 @@
+package com.back.security.auth;
+
+public class JwtConfig {
+	public static final String LLAVE_SECRETA = "";
+	
+	public static final String RSA_PRIVADA = "";
+	
+	public static final String RSA_PUBLICA = "";
+
+}
